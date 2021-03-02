c120703d027ebf9decea0d7232300587703b97a0
Carolyn <carolyngychan@gmail.com>
2021-03-01 16:27:06 -0800

    Added date_followed attribute

--- app/takehome/models.py
+++ app/takehome/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 class Subreddit(models.Model):
     """ A subreddit must have:
         - Identifying name (ex: 'AskReddit'). Limited by 180 chars
+        - Date followed, so that we can evict the oldest if necessary
     """
     name = models.CharField(max_length=180)
+    date_followed = models.DateTimeField(default=timezone.now)

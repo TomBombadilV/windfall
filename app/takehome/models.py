456df96424d37b79699ebad50a4dd7a39b4d332a
Carolyn <carolyngychan@gmail.com>
2021-03-01 18:27:11 -0800

    Added string representation

--- app/takehome/models.py
+++ app/takehome/models.py
@@ -8,3 +8,6 @@ class Subreddit(models.Model):
     """
     name = models.CharField(max_length=180)
     date_followed = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
